a109844672e68ee4a5f82b1f6921ce820a917ee9
Avnish Chauhan <avnishchauhan065@gmail.com>
2023-05-31 00:51:52 +0530

    dockerize application

--- database.js
+++ database.js
@@ -1,20 +1,36 @@
-const mysql = require("mysql");
+const mysql = require('mysql2');
 
-const connection = mysql.createConnection({
-  host: process.env.MYSQL_HOST,
-  port: "3306",
-  user: process.env.MYSQL_USER,
-  database: process.env.MYSQL_DB,
-  password: process.env.MYSQL_PASS
-});
+let connection;
 
-connection.connect((err) => {
-  if (err) {
-    console.log("Error connecting to db");
-    console.log(err);
-    return;
-  }
-  console.log("Connection established");
-});
+const connectionConfigs = {
+	host: process.env.MYSQL_HOST,
+	port: '3306',
+	user: process.env.MYSQL_USER,
+	database: process.env.MYSQL_DATABASE,
+	password: process.env.MYSQL_ROOT_PASSWORD,
+};
+
+function connectWithRetry() {
+	connection = mysql.createConnection(connectionConfigs);
+
+	connection.connect((err) => {
+		if (err) {
+			console.err('Error connecting to db', err);
+			console.log('Retrying in 5 seconds');
+			setTimeout(connectWithRetry, 5000);
+			return;
+		}
+		console.log('Connection established');
+	});
+
+	connection.on('error', (err) => {
+		console.log('db error', err);
+		if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+			connectWithRetry();
+		} else {
+			throw err;
+		}
+	});
+}
 
 module.exports = connection;
